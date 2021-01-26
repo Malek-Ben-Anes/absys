4877174cfc5deb23ba4b3bc86b23021e182bc520
Omar-86 <benanesmalek@gmail.com>
2021-01-26 20:24:59 +0100

    airbus: implementing create user unit test

--- back/src/test/java/com/absys/test/service/UserServiceTest.java
+++ back/src/test/java/com/absys/test/service/UserServiceTest.java
@@ -1,9 +1,9 @@
 package com.absys.test.service;
 
 import com.absys.test.dto.UserDto;
+import com.absys.test.dto.request.CreateUserRequest;
 import com.absys.test.model.UserEntity;
 import com.absys.test.repository.UserRepository;
-import com.absys.test.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,7 +31,24 @@ public class UserServiceTest {
     UserService userService;
 
     @Test
-    public void findAll() {
+    public void should_createNewUser() {
+        when(userRepository.save(any())).thenReturn(new UserEntity("JHXZ677", "JOHN", "DOE", new Date(), "TEXAS", "CRAFTER", CREATED));
+
+        CreateUserRequest request = new CreateUserRequest("JOHN", "DOE", new Date(), "TEXAS", "CRAFTER");
+        //test
+        UserDto userDto = userService.createUser(request);
+
+        assertEquals("JHXZ677", userDto.getId());
+        assertEquals("JOHN", userDto.getFirstName());
+        assertEquals("DOE", userDto.getLastName());
+        assertEquals("TEXAS", userDto.getEarthCountry());
+        assertEquals("CRAFTER", userDto.getEarthJob());
+        assertEquals(CREATED, userDto.getStatus());
+        verify(userRepository, times(1)).save(any());
+    }
+
+    @Test
+    public void should_findAllUsers() {
         UserEntity userOne = new UserEntity("JHXZ677", "JOHN", "DOE", new Date(), "TEXAS", "CRAFTER", CREATED);
         UserEntity userTwo = new UserEntity("JHXZ678", "JOHN1", "DOE1", new Date(), "TEXAS", "CRAFTER", DONE);
         UserEntity userThree = new UserEntity("JHXZ679", "JOHN2", "DOE2", new Date(), "TEXAS", "CRAFTER", DONE);
