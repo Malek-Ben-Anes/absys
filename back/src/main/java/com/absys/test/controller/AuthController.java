bc405b2145163dce0e660a7d20982a579fa00135
Omar-86 <benanesmalek@gmail.com>
2021-01-26 23:15:00 +0100

    airbus: integrate swagger to back-end

--- back/src/main/java/com/absys/test/controller/AuthController.java
+++ back/src/main/java/com/absys/test/controller/AuthController.java
@@ -3,9 +3,15 @@ package com.absys.test.controller;
 import com.absys.test.dto.UserDto;
 import com.absys.test.dto.request.LoginRequest;
 import com.absys.test.service.AuthService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -16,6 +22,13 @@ public class AuthController {
 
     private final AuthService authService;
 
+    @ApiOperation(value = "Login client with registrationId", nickname = "login", response = UserDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User has been successfully logged in."),
+            @ApiResponse(code = 400, message = "Invalid request: given registrationId might be blank."),
+            @ApiResponse(code = 401, message = "User could not log in."),
+            @ApiResponse(code = 500, message = "An error occurred server-side.")
+    })
     @PostMapping("/login")
     public ResponseEntity<UserDto> login(@Valid @RequestBody LoginRequest loginRequest) {
         return ResponseEntity.ok(authService.login(loginRequest));
--- back/src/main/java/com/absys/test/controller/UserController.java
+++ back/src/main/java/com/absys/test/controller/UserController.java
@@ -4,6 +4,9 @@ import com.absys.test.dto.JobAndCountryUserGroupDto;
 import com.absys.test.dto.UserDto;
 import com.absys.test.dto.request.CreateUserRequest;
 import com.absys.test.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +22,12 @@ public class UserController {
 
     private final UserService userService;
 
+    @ApiOperation(value = "Create new user", nickname = "registerUser", response = UserDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "User has been successfully created."),
+            @ApiResponse(code = 400, message = "Invalid request: one field might be blank."),
+            @ApiResponse(code = 500, message = "An error occurred server-side.")
+    })
     @PostMapping
     public ResponseEntity<UserDto> registerUser(@Valid @RequestBody CreateUserRequest createRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(createRequest));
@@ -26,10 +35,17 @@ public class UserController {
 
     @GetMapping
     public ResponseEntity<List<UserDto>> findAll() {
-        // TODO paginate the response
+        // TODO It would be better to return a paginated response instead of a complete list.
         return ResponseEntity.ok(userService.findAll());
     }
 
+    @ApiOperation(value = "Create new user", nickname = "workflow", response = UserDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Registration User Workflow has been successfully executed."),
+            @ApiResponse(code = 400, message = "Invalid request: one field might be blank."),
+            @ApiResponse(code = 404, message = "User not found."),
+            @ApiResponse(code = 500, message = "An error occurred server-side.")
+    })
     @PatchMapping("/{userId}/workflow")
     public ResponseEntity<UserDto> workflow(@PathVariable("userId") String userId) {
         return ResponseEntity.ok(userService.workflow(userId));
